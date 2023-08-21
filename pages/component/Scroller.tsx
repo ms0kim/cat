3c749cbb48847d33485959dc1f4599e9fc721272
Sjuwon <102498237+Sjuwon@users.noreply.github.com>
2023-08-21 14:21:48 +0900

    song

--- pages/component/Scroller.tsx
+++ pages/component/Scroller.tsx
@@ -1,36 +1,44 @@
-import { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import SmoothScrollbar from 'smooth-scrollbar';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 
-export default function Layout({ children, ...rest }) {
-  let $content = useRef();
-  let scrollbar = useRef();
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children, ...rest }: LayoutProps): JSX.Element {
+  const $content = useRef<HTMLDivElement>(null);
+  const scrollbar = useRef<SmoothScrollbar | null>(null);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
     const el = $content.current;
 
-    scrollbar.current = SmoothScrollbar.init(el, {
-      // renderByPixels: false,
-      damping: 0.075,
-      delegateTo: document
-    });
+    if (el) {
+      scrollbar.current = SmoothScrollbar.init(el, {
+        // renderByPixels: false,
+        damping: 0.075,
+        delegateTo: document,
+      });
 
-    scrollbar.current.setPosition(0, 0);
-    scrollbar.current.track.xAxis.element.remove();
+      scrollbar.current.setPosition(0, 0);
+      scrollbar.current.track.xAxis.element.remove();
 
-    ScrollTrigger.scrollerProxy(el, {
-      scrollTop(value) {
-        if (arguments.length) {
-          scrollbar.current.scrollTop = value;
-        }
-        return scrollbar.current.scrollTop;
-      }
-    });
+      ScrollTrigger.scrollerProxy(el, {
+        scrollTop(value?: number) {
+          if (typeof value !== 'undefined') {
+            if (scrollbar.current) {
+              scrollbar.current.scrollTop = value;
+            }
+          }
+          return scrollbar.current ? scrollbar.current.scrollTop : 0;
+        },
+      });
 
-    scrollbar.current.addListener(ScrollTrigger.update);
+      scrollbar.current.addListener(ScrollTrigger.update);
+    }
 
     return () => {
       if (scrollbar.current) {
