c729834ed35b468820a9e50128f5d38e6619d2d1
Sjuwon <102498237+Sjuwon@users.noreply.github.com>
2023-08-21 18:45:17 +0900

    jkk

--- pages/api/photo.ts
+++ pages/api/photo.ts
@@ -77,6 +77,4 @@ export default function handler(req:NextApiRequest, res:NextApiResponse) {
   } else {
     res.status(405).json({message: 'Method not allowed'});
   }
-
-  res.status(200).json(photos);
 }
