8b6a812fd7be3be54b594d554277a8cb38b21447
Sjuwon <102498237+Sjuwon@users.noreply.github.com>
2023-08-22 13:16:24 +0900

    xx

--- pages/api/photo.ts
+++ pages/api/photo.ts
@@ -51,31 +51,43 @@ const photos: Photo[] = [
   },
 ];
 
+function corsMiddleware(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // 모든 origin에서 접근 허용 (보안에 취약할 수 있음, 실제 배포에서는 특정 origin만 허용하는 것이 좋습니다)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // 허용하는 HTTP 메서드 목록
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // 요청 헤더 중 Content-Type을 허용
+}
+
 export default function handler(req:NextApiRequest, res:NextApiResponse) {
-  if(req.method === 'GET'){
-    res.status(200).json(photos);
-  } else if(req.method === 'POST'){
-    const {title, image, extension} = req.body;
+  corsMiddleware(req, res);
+  
+  if (req.method === 'POST') {
+    // POST 요청 처리
+    const { title, image, extension } = req.body;
     const imageBuffer = Buffer.from(image, 'base64');
     const imageFileName = `${Date.now()}.${extension}`;
     const imagePath = path.join(process.cwd(), 'public', 'uploads', imageFileName);
 
-    fs.writeFile(imagePath, imageBuffer, (err) => {
-      if(err) {
-        console.error('Error uploading image:', err);
-        res.status(500).json({message: 'Error uploading image'});
-      } else {
-        const newPhoto: Photo = {
-          id: Date.now().toString(),
-          title: title,
-          img: `/uploads/${imageFileName}`,
-        };
+    try {
+      // 이미지 저장
+      fs.writeFileSync(imagePath, imageBuffer);
 
-        photos.unshift(newPhoto); //최근 등록순
-        res.status(201).json(newPhoto);
-      }
-    });
+      const newPhoto = {
+        id: Date.now().toString(),
+        title: title,
+        img: `/uploads/${imageFileName}`,
+      };
+
+      photos.unshift(newPhoto); // 최근 등록순
+      res.status(201).json(newPhoto);
+    } catch (error) {
+      console.error('Error uploading image:', error);
+      res.status(500).json({ message: 'Error uploading image' });
+    }
+  } else if (req.method === 'GET') {
+    // GET 요청 처리
+    res.status(200).json(photos);
   } else {
-    res.status(405).json({message: 'Method not allowed'});
+    // 허용되지 않는 HTTP 메서드에 대한 처리
+    res.status(405).json({ message: 'Method not allowed' });
   }
 }
